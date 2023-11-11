4f3f65a85323d4c0499b8466167481960e083fd8
acanales3 <alexander.s.canales@gmail.com>
2023-11-11 15:36:59 -0600

    Added About Me Page Complete

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,129 @@
 "use client";
 
 export function AboutMe() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen bg-[#FCE2D4] text-[#A75D5D]">
+      <div className="flex flex-col justify-center items-center w-full lg:h-[118%] md:h-[130%] sm:h-[145%] h-[170%]">
+        <div className="text-[#915454] text-4xl sm:text-7xl font-bold lg:mt-80 md:mt-[450px] sm:mt-[525px] mt-[650px]">
+          <h1>About Me</h1>
+        </div>
+        <div className="py-4">
+          <p className="px-8 max-w-[1000px]">
+            Hello my name is Alex Canales. I am a passionate sophmore Computer
+            Science major at Texas Christian University, with a minor in
+            Mathematics and Philosophy. However, even though I go to school in
+            Texas, I am originally from Brownsburg, IN. I fell in love with
+            Computer Science taking classes like Computer Science Principles,
+            Computer Science Applications, Digital Electronics, and
+            Cybersecurity. Some other classes that I took in High School that I
+            took that gave me a well-rounded preparation for college was
+            Calculus BC and Physics Mechanics: C. It was not until the end of my
+            senior year of high school that I decided to take the opportunity
+            given to me at Texas Christian University to pursue higher
+            education.
+          </p>
+        </div>
+        <div className="text-2xl sm:text-4xl font-bold py-4">
+          High School Achomplishments
+        </div>
+        <p className="px-8 max-w-[1000px]">
+          I graduated in 2022 at the top of my class, and wanted to pursue
+          Computer Science in College from the classes I took from High School.
+          I was a part of National Honors Society with a cumulative GPA of 4.6
+          on a 4.0 scale. High School acitivies I was a part of was a tutoring
+          program that my school ran. Some hobbies that I picked up in High
+          School included skiing, fishing, and playing the guitar. I fell in
+          love with these things and tried to stick with them as much as I could
+          growing up, and continue them in college. I was involved with the
+          wrestling team at my High School as well. My Junior year, we finished
+          12th in the nation and I was proud to be a wrestler on that team.
+        </p>
+        <div className="text-2xl sm:text-4xl font-bold py-4">
+          Collegiate Achomplishments
+        </div>
+        <p className="px-8 max-w-[1000px]">
+          At Texas Christian University, I have completed two semesters. Within
+          these two semesters, I was able to make it on the Dean&#39;s Honors
+          List for the TCU College of Science and Engineering for both semesters
+          of my Freshman Year. Along with this, I was able to maintain a 4.0 GPA
+          my second semester of my Freshman Year.
+        </p>
+        <div className="text-2xl sm:text-4xl font-bold py-4">
+          Major Relevant Coursework
+        </div>
+        <div className="px-8 max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full">
+          <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-4">
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Fall Semester 2022
+              <div className="py-4">
+                <ul>
+                  <li>COSC 10403: Intro to Programming</li>
+                  <li>MATH 20123: Discrete Mathematics I</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500 py-2">
+              Spring Semester 2023
+              <div className="py-4">
+                <ul>
+                  <li>COSC 20203: Techniques in Programming</li>
+                  <li>MATH 10043: Elementary Statistics</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Fall Semester 2023
+              <div className="py-4">
+                <ul>
+                  <li>COSC 20803: Data Structures & Algorithms</li>
+                  <li>CITE 30103: Unix/Linux System Administration</li>
+                  <li>MATH 30224: Linear Algebra</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Spring Semester 2024
+              <div className="py-4">
+                <ul>
+                  <li>N/A</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Fall Semester 2024
+              <div className="py-4">
+                <ul>
+                  <li>N/A</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Spring Semester 2025
+              <div className="py-4">
+                <ul>
+                  <li>N/A</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Fall Semester 2025
+              <div className="py-4">
+                <ul>
+                  <li>N/A</li>
+                </ul>
+              </div>
+            </div>
+            <div className="shadow-md shadow-[#FFC3A1] hover:scale-110 duration-500">
+              Spring Semester 2026
+              <div className="py-4">
+                <ul>
+                  <li>N/A</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
