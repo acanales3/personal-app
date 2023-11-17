7fe73db6521e9f71039729e77009549a7f8e0654
acanales3 <alexander.s.canales@gmail.com>
2023-11-17 17:23:00 -0600

    Final Social Media

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -5,8 +5,8 @@ import { FaGithub, FaLinkedin, FaFacebook, FaInstagram } from "react-icons/fa";
 
 export function SocialMedia() {
   return (
-    <div className="w-full h-screen bg-[#FCE2D4] text-[#A75D5D]">
-      <div className="max-w-[800px] mx-auto p-2 flex flex-col justify-center w-full lg:h-[118%] md:h-[130%] sm:h-[145%] h-[180%]">
+    <div className="w-full h-screen bg-[#FCE2D4] text-[#A75D5D] pt-16">
+      <div className="max-w-[800px] mx-auto p-2 flex flex-col justify-center w-full">
         <div className="pb-2">
           <h1 className="text-4xl font-bold">Social Media</h1>
           <p className="py-2">Here are some of my other Socials!</p>
@@ -16,8 +16,8 @@ export function SocialMedia() {
         <div className="grid sm:grid-cols-2 gap-4">
           <div>
             <Link href="https://github.com/acanales3" target="_blank">
-              <div className="bg-[#AAAAAA] shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
-                <div className="relative text-black">
+              <div className="bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
+                <div className="relative text-white">
                   <FaGithub
                     size={100}
                     className="absolute inset-0 m-auto opacity-100 group-hover:opacity-0"
@@ -35,7 +35,7 @@ export function SocialMedia() {
               href="https://www.linkedin.com/in/alex-canales-1aa8ab27b"
               target="_blank"
             >
-              <div className="bg-[#AAAAAA] shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
+              <div className="bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500 shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
                 <div className="relative text-[#0077B5]">
                   <FaLinkedin
                     size={100}
@@ -54,7 +54,7 @@ export function SocialMedia() {
               href="https://www.facebook.com/profile.php?id=100086728875505"
               target="_blank"
             >
-              <div className="bg-[#AAAAAA] shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
+              <div className="bg-gradient-to-bl from-sky-400 to-blue-500  shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
                 <div className="relative text-[#3B5998]">
                   <FaFacebook
                     size={100}
@@ -73,8 +73,8 @@ export function SocialMedia() {
               href="https://www.instagram.com/_alexcanales/"
               target="_blank"
             >
-              <div className="bg-[#AAAAAA] shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
-                <div className="relative text-[#cd486b]">
+              <div className="bg-gradient-to-bl from-pink-500 via-red-500 to-yellow-500 shadow-lg group container rounded-md flex justify-center items-center mx-auto content-div">
+                <div className="relative text-white">
                   <FaInstagram
                     size={100}
                     className="absolute inset-0 m-auto opacity-100 group-hover:opacity-0"
