bf26a6612a57d09dc327decb2c15f5f9126c36f3
acanales3 <alexander.s.canales@gmail.com>
2023-11-17 10:11:21 -0600

    Creation of Contact Form

---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export function Contact() {
+  return (
+    <div className="w-full h-screen bg-[#FCE2D4] flex justify-center items-center p-4">
+      <form className="flex flex-col max-w-[600px] w-full">
+        <div>
+          <p>Contact Me</p>
+        </div>
+      </form>
+    </div>
+  );
+}
