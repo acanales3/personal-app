f3a296907ba2b41efd4ef3fa67ec38044e675ec7
acanales3 <alexander.s.canales@gmail.com>
2023-11-17 17:22:02 -0600

    Final Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,8 @@ import { AboutMe } from "@/components/AboutMe";
 import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
 import { SocialMedia } from "@/components/SocialMedia";
+import { ContactRev } from "@/components/ContactRev";
 
 export default function Page() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <Home />;
 }
